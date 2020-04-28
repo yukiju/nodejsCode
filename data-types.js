fa279369f3c640dd8d9ed896ab4c9b949a2b2f82
milton-reyes <altf4616@outlook.com>
2020-04-28 11:37:36 -0400

    changes

---
+++ data-types.js
@@ -0,0 +1,22 @@
+let x = 10;
+let str = 'Hello world';
+let bool = true;
+let arr = [1, 2, 3];
+let date = Date();
+let symbol = Symbol('myApp');
+/*
+    JavaScript Types
+    1. Number = (Java Double)
+    2. string
+    3. boolean
+    4. object
+    5. symbol
+
+*/
+
+console.log(typeof x); // number
+console.log(typeof str); // string
+console.log(typeof bool); //boolean
+console.log(typeof arr); // object
+console.log(typeof date); // string
+console.log(typeof symbol); // symbol
