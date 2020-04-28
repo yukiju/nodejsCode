cfe79f0839e3ba96ad192d25d58fb04b21fe57d8
milton-reyes <altf4616@outlook.com>
2020-04-28 15:53:52 -0400

    changes

--- objects.js
+++ objects.js
@@ -17,8 +17,51 @@ console.log(person.favoriteFood);
 
 // creates two variables referencing the same object
 const otherPerson = person;
+// Spread operator
+const newPerson = { ...person };
 
 otherPerson.firstName = 'Billy';
 console.log(person.firstName); // Billy
 
+// Spread Operator - ...
+console.log(newPerson.firstName); // Abby
+
+console.log(otherPerson === person);
+console.log(newPerson === person);
+
+// this is assigning new values to person
+const billy = {
+    ...person,
+    //this is actually doing
+    // firstName: 'Billy',
+    // lastName: 'Adams',
+    // favoriteFood: 'Ice Cream'
+    // then substituing the values
+    firstName: 'Billy',
+    lastName: 'Brown',
+};
+
+console.log(billy);
+
+const a = {
+    a: 'apple'
+};
+const b = {
+    b: 'banana'
+};
+const c = {
+    ...a,
+    ...b
+}
+
+console.log(c);
+
+
+
+// Spread operator also works with arrays
+
+const arr = [1,2,3];
+const otherArr = [4,5,6];
+const combinedArray = [...arr, ...otherArr];
+console.log(combinedArray);
 
