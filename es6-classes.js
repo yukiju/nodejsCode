de18ebbdd860e2431de1371c2aa56b0692857fb1
milton-reyes <altf4616@outlook.com>
2020-04-29 11:56:47 -0400

    added es6-classes.js

---
+++ es6-classes.js
@@ -0,0 +1,5 @@
+/*
+    ES6 classes are a major feature of the EcmaScript 6 version release.
+    ES6 classes are a syntax that provides more familiar approach to object
+    inheritance models that features a 'class' like syntax.
+*/
